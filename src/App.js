1e2b434fdc4a105e83e7521dbedab1592fa71ae1
GB-Navarro <gbnavarro@usp.br>
2022-06-29 22:59:54 -0300

    Basic routes has been created and a new context has been added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import GlobalStyles from "./globalStyles"
 import SignUp from "./components/SignUp"
 import SignIn from "./components/SignIn"
 
+import TokenContext from "./contexts/TokenContext";
+
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App(){
+
+  const [token, setToken] = useState("");
+
   return(
     <>
       <GlobalStyles />
-      <SignIn />
-      {/*<SignUp />*/}
+      <TokenContext.Provider value={{ token, setToken }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SignIn />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </TokenContext.Provider>  
     </>
   )
 }
